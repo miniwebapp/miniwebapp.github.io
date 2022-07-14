1c03fc6a864165fcbcc303724a5fd071766883f3
miniwebapp <92636104+miniwebapp@users.noreply.github.com>
2022-07-14 18:44:12 +0530

    Add files via upload

---
+++ assets/About.95d01723.js
@@ -0,0 +1,1 @@
+import{j as t,F as r}from"./index.b6b2f139.js";function o(){return t(r,{})}export{o as default};
---
+++ assets/ArrowBack.30b813bb.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index.b6b2f139.js";import{c as a}from"./dividerClasses.0c29ded8.js";var t=a(r("path",{d:"M20 11H7.83l5.59-5.59L12 4l-8 8 8 8 1.41-1.41L7.83 13H20v-2z"}),"ArrowBack");export{t as A};
