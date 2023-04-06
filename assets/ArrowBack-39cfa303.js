e33dfe8864d4875d2782097c51d491fdd14c28bb
miniwebapp <92636104+miniwebapp@users.noreply.github.com>
2023-04-06 23:20:16 +0530

    Add files via upload

---
+++ assets/ArrowBack-39cfa303.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-b8c10478.js";import{E as o}from"./App-b16039b3.js";const s=o(r.jsx("path",{d:"M20 11H7.83l5.59-5.59L12 4l-8 8 8 8 1.41-1.41L7.83 13H20v-2z"}),"ArrowBack");export{s as A};
---
+++ assets/Updates-c31a8ef5.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-b8c10478.js";import{R as a,T as s,m as o}from"./App-b16039b3.js";function n(){return r.jsxs(r.Fragment,{children:[r.jsx(a,{}),r.jsx(s,{py:"20%",color:"secondary.main",fontFamily:"cursive",variant:"h3",align:"center",children:"No updates"}),r.jsx(o,{})]})}export{n as default};
