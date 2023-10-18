6e11a9cd9c8c309ad069a6c5d1238b2de97aa133
miniwebapp <92636104+miniwebapp@users.noreply.github.com>
2023-10-18 17:08:35 +0530

    Add files via upload

---
+++ assets/ELogin-add09942.js
@@ -0,0 +1,1 @@
+import{j as t}from"./index-64b38d72.js";function i(n){return n.i,n.e,t.jsx(t.Fragment,{children:'"login"'})}export{i as default};
---
+++ assets/Updates-c11f6608.js
@@ -0,0 +1,1 @@
+import{j as a,R as r,T as s,F as e}from"./index-64b38d72.js";function n(){return a.jsxs(a.Fragment,{children:[a.jsx(r,{}),a.jsx(s,{py:"20%",color:"secondary.main",fontFamily:"cursive",variant:"h3",align:"center",children:"No updates"}),a.jsx(e,{})]})}export{n as default};
