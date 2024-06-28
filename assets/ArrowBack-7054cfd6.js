248a9dc6ff84f671bc5a9a9834c76127086fbe93
miniwebapp <92636104+miniwebapp@users.noreply.github.com>
2024-06-29 01:23:06 +0530

    Add files via upload

---
+++ assets/ArrowBack-7054cfd6.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-459c5962.js";import{c as o}from"./appStore-653fb111.js";const s=o(r.jsx("path",{d:"M20 11H7.83l5.59-5.59L12 4l-8 8 8 8 1.41-1.41L7.83 13H20v-2z"}),"ArrowBack");export{s as A};
